831dda6998746671a7f64e8b21c812bbbfa135da
jheyson <8410@itescam.edu.mx>
2024-03-01 16:05:59 -0600

    actualizacion

--- Nombres.java
+++ Nombres.java
@@ -1,9 +1,29 @@
 public class Nombres{
 
-    String nombre;
-    int maticula;
+   private String nombre;
+    private String matricula;
+    
+    public void setnombre(String nombre){
+        this.nombre=nombre;
+    }
+    public String getnombre(){
+        return nombre;
+    }
+
+    public void setmatricula(String matricula){
+        this.matricula=matricula;
+    }
+    public String getmatricula(){
+        return matricula;
+    }
+
     public static void main(String [] args){
         System.out.println("carlos eduardo pech ortiz   8368");
+
+        Nombres nombres = new Nombres();
+        nombres.setnombre("jheyson anuar chim chan");
+        nombres.setmatricula("8410");
+        System.out.println(nombres.getnombre() + " " + nombres.getmatricula());
     }
 }
 
