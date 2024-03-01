3f66e14e5d6523e25eb598c5384f17e2716802c6
CarlosEduardoPechOrtiz <8368@itescam.edu.mx>
2024-03-01 15:48:50 -0600

    cambio

--- Nombres.java
+++ Nombres.java
@@ -3,7 +3,7 @@ public class Nombres{
     String nombre;
     int maticula;
     public static void main(String [] args){
-        
+        System.out.println("carlos eduardo pech ortiz   8368");
     }
 }
 
